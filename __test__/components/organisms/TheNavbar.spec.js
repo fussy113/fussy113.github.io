afcc7bca25f858d6654bdbc02b4127823549e834
fussy113 <s1220080@gmail.com>
2020-01-25 22:56:14 +0900

    test

--- __test__/components/organisms/TheNavbar.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import TheNavbar from '@/components/organisms/TheNavbar.vue'
-
-describe('TheNavbar', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(TheNavbar)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
