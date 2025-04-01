8a3e7dc8a21bd6dc64805d69e97c514b6d5b11b3
fussy113 <s1220080@gmail.com>
2025-04-01 08:03:35 +0000

    bump tailwindcss v4 using vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), VueDevTools(), tailwindcss()],
   css: {
     preprocessorOptions: {
       scss: {
@@ -19,7 +17,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
